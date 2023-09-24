e0944ef35afe05a4392345230b14dff0598f3e22
HasanSadikin <hasanmcmissile2711@gmail.com>
2023-09-24 15:27:44 +0800

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -66,7 +66,7 @@ window.addEventListener('scroll', handleScroll)
                 class="w-10 h-10 cursor-pointer md:hidden" @click="toggleMenu">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             </svg>
-        </div>
+        </div> 
     </div>
 </template>
 
